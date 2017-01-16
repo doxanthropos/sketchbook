6a7b9715770c5608a263ce2bb222b62a86ddcc01
Knut Jackowski <doxanthropos@googlemail.com>
2017-01-16 21:16:32 +0100

    made a version of the game from funprogramming.org 60

---
+++ funprogramming/60a/sketch.js
@@ -0,0 +1,31 @@
+var diam = 100;
+var a = 0;
+var playing = false;
+function setup() {
+  createCanvas(500,400);
+  background(0);
+  noFill();
+}
+
+function draw() {
+  if(playing){
+  background(0);
+  var x = noise(a, 10)*width;
+  var y = noise(a, 20)*height;
+  stroke(255,255,0);
+  ellipse(x,y,diam,diam);
+  stroke(0,255,0);
+  ellipse(mouseX,mouseY,diam,diam);
+  if(dist(mouseX,mouseY,x,y)<diam){
+    playing = false;
+  }
+  a = a + 0.01;
+  } else {
+    background(0);
+  }
+}
+
+function mousePressed(){
+  playing = true;
+}
+

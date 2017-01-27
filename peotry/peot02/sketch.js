7ca34f4442bbeb741361786f325c3b46a9e7bd1c
Knut Jackowski <doxanthropos@googlemail.com>
2017-01-27 15:56:09 +0100

    A second peot

---
+++ peotry/peot02/sketch.js
@@ -0,0 +1,85 @@
+var st;
+var chain = {};
+var order = 1;
+var lines;
+var start;
+var seeder;
+var peotPara;
+var newPeom;
+var peomUrl;
+var myUrl;
+var baseUrl;
+var currentU;
+
+function preload(){
+  lines = loadStrings("dunwich1.txt");
+}
+function setup() {
+  noCanvas();
+  noLoop();
+  currentU = window.location.href;
+  if(currentU.indexOf("?v=") < 0){
+    baseUrl = currentU;
+  } else {
+    baseUrl = currentU.substring(0,currentU.indexOf("?v="));
+    }
+}
+
+function draw() {
+   for(var j = 0;j<lines.length-order;j++){
+     st = lines[j].split(" ");
+     if(st.length>order){
+     for(var i = 0;i<st.length-order;i++){
+      var gram = String(st[i]);
+      if(gram=="watch"){ console.log("Gram: " + st[i]+ " Zeug: " + st[i+1]); }
+      if(chain[gram]&&Array.isArray(chain[gram])){
+        chain[gram].push(String(st[i+1]));
+      } else {
+        chain[gram] = [String(st[i+1])];
+      }
+    }
+   }
+  }
+  peotPara = createP();
+  newPeom = createButton("Click here for a new Peom");
+  peomUrl = createP();
+  if(currentU.indexOf("?v=") < 0){
+    seeder = round(random(1000000));
+  } else {
+    seeder = currentU.substring(currentU.indexOf("?v=")+3,currentU.length);
+    } 
+  generateParagraph();
+  newPeom.mousePressed(newParagraph);
+}
+
+function newParagraph(){
+  seeder = round(random(1000000));
+  generateParagraph();
+}
+
+function generateParagraph(){
+   
+  randomSeed(seeder);
+  myUrl = baseUrl + "?v=" + seeder;
+  start = ["When"];
+  for(var i = 0;i<30;i++){
+    var gram = start[start.length -1];
+    console.log(gram);
+    if(chain[gram]){
+    start.push(random(chain[gram]));
+    console.log("Start ist: " + start);
+    } else { start.pop();i--;}
+  }
+  for(var i = 0;i<start.length;i++){
+    if(start[i]=="."){
+      start = start.substring(0,i+1) + "\n" + start.substring(i+1,start.length);
+      i++;
+    }
+  }
+  peotPara.html(start.join(" ") + ".");
+  console.log(seeder);
+ 
+  peomUrl.html("Permalink to this Peom: " + myUrl);
+}
+
+/* try this one: 350996 */

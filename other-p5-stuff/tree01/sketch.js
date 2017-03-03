894fb5a55de8ee4a3c4a7e34d7e1484310ff65f6
Knut Jackowski <doxanthropos@googlemail.com>
2017-03-03 15:32:35 +0100

    growing roots

---
+++ other-p5-stuff/tree01/sketch.js
@@ -0,0 +1,28 @@
+var cr;
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(0);
+  noStroke();
+  noLoop();
+  cr = random(200000);
+  randomSeed(cr);
+}
+
+function draw() {
+  branch(width/2,0,0,20);
+}
+
+function branch(x,y,d,s){
+  if(s<=1){
+    return 0;
+  } else {
+    if(random(10)>9){
+      ellipse(x,y,s,s);
+      branch(x-d,y+2,d-1,s-2);
+      branch(x+d,y+2,d+1,s-2);
+    } else {
+      ellipse(x+d,y,s,s);
+      branch(x+d,y+2,d,s);
+    }
+  }
+}

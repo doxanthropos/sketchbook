508fa8e3630c252140ae7d26bef314c71c1f5a0c
Knut Jackowski <doxanthropos@googlemail.com>
2017-04-14 11:11:12 +0200

    loading a sound into web audio api

---
+++ 2017041401/sketch.js
@@ -0,0 +1,31 @@
+var audio_context = window.AudioContext || window.webkitAudioContext;
+
+var con = new audio_context();
+
+var snare;
+
+loadSample('sounds/snare.wav', function(buffer){
+  snare = buffer;
+});
+
+function playSnare(){
+  var player = con.createBufferSource();
+  player.buffer = snare;
+  player.start();
+  player.loop = false;
+  player.connect(con.destination);
+}
+
+function loadSample(url,callback){
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.responseType = 'arraybuffer';
+  request.onload = function(){
+    var audioData = request.response;
+    con.decodeAudioData(audioData, function(buffer){
+      console.log(buffer);
+      callback(buffer);
+    });
+  };
+  request.send();
+}

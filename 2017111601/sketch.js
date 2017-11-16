75d7c4b976817db37c69ea91a4c5a2d719b6c703
Knut Jackowski <doxanthropos@googlemail.com>
2017-11-16 22:37:01 +0100

    First steps into making bookmarklets and browser extensions

---
+++ 2017111601/sketch.js
@@ -0,0 +1,10 @@
+/*
+ * 11.1: Introduction to Chrome Extensions
+ * https://www.youtube.com/watch?v=hkOTAmmuv_4
+ * 11.2: Bookmarklets
+ * https://www.youtube.com/watch?v=DloHqUfPbJc&t=215s
+*/
+
+(function(){
+  console.log("Anonymus function running!");
+})();

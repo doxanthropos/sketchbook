5c5e7c4e013faa76cbb89b3a4735bbac0be47331
Knut Jackowski <doxanthropos@googlemail.com>
2018-01-19 22:15:57 +0100

    Followed a WebRTC tutorial

---
+++ 2018011901/sketch.js
@@ -0,0 +1,21 @@
+var video_out = document.getElementById("vid-box");
+
+function login(form) {
+	var phone = window.phone = PHONE({
+	    number        : form.username.value || "Anonymous", // listen on username line else Anonymous
+	    publish_key   : 'pub-c-dd995222-e97e-4861-a549-dc4355a10f64',
+	    subscribe_key : 'sub-c-0b94ad20-fd5b-11e7-ad25-0a073915cf0b',
+	});	
+	phone.ready(function(){ form.username.style.background="#55ff5b"; });
+	phone.receive(function(session){
+	    session.connected(function(session) { video_out.appendChild(session.video); });
+	    session.ended(function(session) { video_out.innerHTML=''; });
+	});
+	return false; 	// So the form does not submit.
+}
+
+function makeCall(form){
+	if (!window.phone) alert("Login First!");
+	else phone.dial(form.number.value);
+	return false;
+}

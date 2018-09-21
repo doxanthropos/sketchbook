d5d8e2df5e4970583a812a89b6dc405e72a4cc7c
Knut Jackowski <doxanthropos@googlemail.com>
2018-09-21 22:45:34 +0200

    Coding Train #115 Snake

---
+++ 2018092101/sketch.js
@@ -0,0 +1,84 @@
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+let gameState = "START";
+let score = 0;
+let highScore = 0;
+function setup() {
+  createCanvas(500,500);
+  w = floor(width/rez);
+  h = floor(height/rez);
+  background(30);
+  frameRate(5)
+  snake = new Snake(w,h);
+  foodLocation();
+}
+function draw() {
+  if(gameState === "START"){
+    drawStart();
+  } else if(gameState === "PLAY"){
+    drawPlay();
+  } else if(gameState === "GAME OVER"){
+    drawGameOver();
+  }
+}
+
+function drawPlay(){
+  scale(rez);
+  background(30);
+  snake.update();
+  snake.show();
+  if(snake.checkDeath(w,h)){
+    gameState = "GAME OVER";
+    score = snake.body.length -1;
+    highScore = max(score, highScore);
+  };
+  if (snake.eat(food)){
+    foodLocation();
+  }
+  fill(200,0,0);
+  rect(food.x, food.y, 1, 1);
+}
+
+function drawStart(){
+  fill(200);
+  noStroke();
+  textSize(width/20);
+  textAlign(CENTER, CENTER)
+  text("Welcome to Snake!", width/2, height/2);
+  text("Click here to play", width/2, height/2 + width/20);
+}
+
+function drawGameOver(){
+  snake = new Snake(w,h);
+  foodLocation();
+  background(30);
+  fill(200);
+  noStroke();
+  textSize(width/20);
+  textAlign(LEFT,TOP);
+  text("Score: " + score, 20, 20);
+  textAlign(RIGHT,TOP);
+  text("Highscore: " + highScore, width-20, 20);
+  textAlign(CENTER, CENTER)
+  text("Game Over", width/2, height/2);
+  text("Click here to play again", width/2, height/2 + width/20);
+}
+
+function keyPressed(){
+  snake.setDir(keyCode);
+}
+
+function foodLocation(){
+  let x = floor(random(w));
+  let y = floor(random(h));
+
+  food = createVector(x, x);
+}
+
+function mousePressed(){
+  print("Mouse Pressed")
+  gameState = "PLAY";
+}
---
+++ 2018092101/snake.js
@@ -0,0 +1,68 @@
+class Snake {
+  constructor(){
+    this.body = [];
+    this.body[0] = createVector(floor(w/2),floor(h/2));
+    this.xdir = 0;
+    this.ydir = 0;
+  }
+  update(){
+    let head = this.body[this.body.length -1].copy();
+    this.body.shift();
+    head.x += this.xdir;
+    head.y += this.ydir;
+    this.body.push(head);
+  }
+  show(){
+    noStroke();
+    fill(200);
+    for(let i = 0; i < this.body.length; i++){
+      rect(this.body[i].x, this.body[i].y, 1, 1);
+    }
+  }
+  setDir(kc){
+    if ( kc === UP_ARROW){
+      this.ydir = -1;
+      this.xdir = 0;
+    } else if ( kc === DOWN_ARROW) {
+      this.ydir = 1;
+      this.xdir = 0;
+    } else if ( kc === LEFT_ARROW) {
+      this.ydir = 0;
+      this.xdir = -1;
+    } else if ( kc === RIGHT_ARROW) {
+      this.ydir = 0;
+      this.xdir = 1;
+    } 
+  }
+
+  eat(pos){
+    let x = this.body[this.body.length-1].x;
+    let y = this.body[this.body.length-1].y;
+    if (x===pos.x && y === pos.y){
+      print("FOOD EATEN");
+      this.grow();
+      return true;
+    } else {
+      return false;
+    }
+  }
+  grow(){
+    let head = this.body[this.body.length -1].copy();
+    this.body.push(head);
+  }
+
+  checkDeath(w,h){
+    let x = this.body[this.body.length -1].x;
+    let y = this.body[this.body.length -1].y;
+    if (x < 0 || x > w -1 || y < 0 || y > h -1){
+      return true;
+    }
+    for (let i = 0; i < this.body.length-1; i++){
+      if (x === this.body[i].x && y === this.body[i].y){
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
